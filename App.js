66087bdfbf5a13080cd0621c62ca44ed7720c5a0
Austin Lanari <email": null>
2016-10-18 10:22:08 -0400

    Remove splus and extra brackets

--- App.js
+++ App.js
@@ -232,11 +232,11 @@ class App extends React.Component {
     $('#timeline-filterables').find('form').remove();
   }
 
-  +  showKeywordForm() {
+    showKeywordForm() {
        //   append form for keyword search to DOM and hide other filters
        $('#timeline-filterables').find('li').hide();
        $('#timeline-filterables').append("<div class='row' id='keywordDiv' style={this.formStyle()}><form class='keyword-search' id='keyword-search-form' type='button' onSubmit={this.keywordSearch}><div class='keyword-search-box input-field col s9'><label for='keywordBox'>Search for bills by keyword</label><input ref='keywordBox' name='keywordBox' id='keywordBox' type='text'/></div><div class='col s3 waves-effect waves-light btn' id='supaDupaButton'><input type='submit' value='search'/></div></form></div>");
-    }       }
+    }       
 
   render() {
     return(
